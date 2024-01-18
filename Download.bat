4b642191c822efba79736293519af012c3cab2b2
Hiếu GL Lite <doanhieudakwil@gmail.com>
2024-01-18 22:50:10 +0700

    Edit GetData File!

--- Download.bat
+++ Download.bat
@@ -37,11 +37,12 @@ Set "EmuInstDir=%ProgramData%\HieuGLLite"
 set "wget=%EmuInstDir%\Bin\wget.exe"
 set "project=EmuInst"
 
+if %1==getdata goto :DLData
+
 echo Hieu GL Lite - Downloader Helper
 echo Version: 1.0
 echo.
 
-if %1==getdata goto :DLData
 
 if exist %Download%\%2.bin goto :ContinueDL
 if not exist %Download%\%2.bin goto :NewDL
